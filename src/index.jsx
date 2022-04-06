6e3eb821c2d61570ea38327d75e7d0a657d22aec
tpikachu <46133358+tpikachu@users.noreply.github.com>
2022-04-05 19:26:14 -0700

    hompage url

--- src/index.jsx
+++ src/index.jsx
@@ -13,17 +13,19 @@ import AdminLayout from "./layouts/Admin";
 // import RTLLayout from "layouts/RTL.js";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path={`auth/*`} element={<AuthLayout />} />
-      <Route path={`admin/*`} element={<AdminLayout />} />
-      {/* <Route path={`/rtl`} element={RTLLayout} /> */}
-      <Route
-        exact
-        path={"/"}
-        element={<Navigate to="/admin/dashboard" replace />}
-      />
-    </Routes>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path={`auth/*`} element={<AuthLayout />} />
+        <Route path={`admin/*`} element={<AdminLayout />} />
+        {/* <Route path={`/rtl`} element={RTLLayout} /> */}
+        <Route
+          exact
+          path={"/"}
+          element={<Navigate to="/admin/dashboard" replace />}
+        />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById("root")
 );
